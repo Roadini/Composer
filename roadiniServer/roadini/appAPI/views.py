9b262f54edee6e5bc8bf284682b30ce0d9208e27
Tiago Ramalho <t.ramalho22@ua.pt>
2018-10-23 01:35:22 +0100

    first api with rest framework django

--- roadiniServer/roadini/appAPI/views.py
+++ roadiniServer/roadini/appAPI/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import PathsTable
+from .serializers import PathsTableSerializer
 
 # Create your views here.
+
+class PathsTableView(viewsets.ModelViewSet):
+    queryset = PathsTable.objects.all()
+    serializer_class = PathsTableSerializer
